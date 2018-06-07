6a705314c0c2cafed201ca4d5a6944490d596762
Borna Relic <borna.relic@gmail.com>
2018-06-07 17:21:29 +0200

    Register controller and view

---
+++ Proxy/Controllers/RegisterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewController.swift
+//  Proxy
+//
+//  Created by Borna Relic on 07/06/2018.
+//  Copyright © 2018 ruaziosteam. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func createAccount(_ sender: UIButton) {
+    }
+    
+}
