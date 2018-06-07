10adbbb3e71dea2d6ab9b5d18d128395461be064
Josipa Lipovac <josipa.lipovac@fer.hr>
2018-06-07 16:02:53 +0200

    Initial Commit

---
+++ Proxy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proxy
+//
+//  Created by Josipa Lipovac on 07/06/2018.
+//  Copyright © 2018 ruaziosteam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
