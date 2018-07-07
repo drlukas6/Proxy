868a145f60a4c2762b83f1a48d0b1a3c2cc9911b
Borna Relic <borna.relic@gmail.com>
2018-07-07 16:41:16 +0200

    About proxy

---
+++ Proxy/Controllers/AboutProxyViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutProxyViewController.swift
+//  Proxy
+//
+//  Created by Borna Relic on 07/07/2018.
+//  Copyright © 2018 ruaziosteam. All rights reserved.
+//
+
+import UIKit
+
+class AboutProxyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = false
+
+    }
+
+}
